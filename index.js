7fe908dbf4d610d3b3f21649714f78dca478e6fd
Jonathan Rehm <jonathan@rehm.me>
2018-08-29 13:12:02 -0700

    Add lint script

---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: 'eslint-config-airbnb-base',
+  rules: {
+    'comma-dangle': [2, 'never'],
+    'new-cap': [
+      2,
+      {
+        capIsNew: false
+      }
+    ],
+    'no-void': 0,
+    'consistent-return': 0,
+    'no-param-reassign': 0,
+    'no-plusplus': [0, { allowForLoopAfterthoughts: true }],
+    'arrow-parens': [0, 'as-needed'],
+    'import/no-unresolved': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/prefer-default-export': 0,
+    'import/extensions': [0, { js: 'never' }]
+  }
+};
